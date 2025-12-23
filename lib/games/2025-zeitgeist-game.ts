74e02a30d14545a0d8d9ad1193355633cb40c60e
Prashant Pawan Pisipati <prashant@renderwolf.ai>
2025-12-22 20:48:09 -0800

    Updated with street view images

--- lib/games/2025-zeitgeist-game.ts
+++ lib/games/2025-zeitgeist-game.ts
@@ -4,7 +4,7 @@ export const zeitgeist2025Game: Game = {
   id: '2025-zeitgeist',
   title: '2025 Zeitgeist',
   description: `Find 2025's most viral moments in this image before the timer runs out!`,
-  backgroundPath: '/static/2025-zeitgeist/zeitgiest_final_image_toy_co.png',
+  backgroundPath: '/static/2025-zeitgeist/zeitgiest2025_master_image_final.png',
   difficulty: 'medium',
   timeLimit: 30,
   shareText: `Can you find 2025's most viral moments in this hidden object game by @renderwolfai?`,
@@ -12,27 +12,27 @@ export const zeitgeist2025Game: Game = {
     {
        id: 'object-1',
        name: 'Gorilla vs 100 men',
-       maskPath: '/static/2025-zeitgeist/zeitgiest_gorilla_trastparent.png'
+       maskPath: '/static/2025-zeitgeist/zeitgiest2025_street_gorilla_transparent.png'
      },
      {
        id: 'object-2',
        name: 'Katy Perry in Space',
-       maskPath: '/static/2025-zeitgeist/zeitgiest_katy_perry_trastparent.png'
+       maskPath: '/static/2025-zeitgeist/zeitgiest2025_street_katyperry_transparent.png'
      },
      {
        id: 'object-3',
-       name: 'Kpop Demon Hunters',
-       maskPath: '/static/2025-zeitgeist/zeitgiest_kpop_demon_hunters_trastparent.png'
+       name: 'KPop Demon Hunters',
+       maskPath: '/static/2025-zeitgeist/zeitgiest2025_street_kpop_transparent.png'
      },
      {
        id: 'object-4',
        name: 'Labubu',
-       maskPath: '/static/2025-zeitgeist/zeitgiest_labubu_trastparent.png'
+       maskPath: '/static/2025-zeitgeist/zeitgiest2025_street_labubu_transparent.png'
      },
      {
        id: 'object-5',
        name: 'Louvre Heist',
-       maskPath: '/static/2025-zeitgeist/zeitgiest_louvre_heist_trastparent.png'
+       maskPath: '/static/2025-zeitgeist/zeitgiest2025_street_louvre_transparent.png'
      },
   ]
 };
